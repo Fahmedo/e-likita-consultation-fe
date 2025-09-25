31404e20872120279913e9f5726f8a4c40905629
Fahmed2024 <100038212+Fahmedo@users.noreply.github.com>
2025-09-25 11:49:05 +0100

    commit change

--- service/environment.ts
+++ service/environment.ts
@@ -1,2 +1,3 @@
 export const baseUrl =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api/v1';
+  process.env.NEXT_PUBLIC_API_URL ||
+  'https://e-likita-consultation-be.onrender.com//api/v1';
